a9fc6d8e794ecaac198aa8c4d2790f1705907bb4
yuto0121 <0121yuto0121@gmail.com>
2025-05-15 22:41:20 +0200

    add: js, css, index.html

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const btn = document.getElementById('helloBtn');
+  btn.addEventListener('click', function() {
+    alert('こんにちは！超簡易サイトへようこそ！');
+  });
+});
+
